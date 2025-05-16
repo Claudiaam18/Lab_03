eaeedac55762545f295c71368d0445f130089dae
Claudiaam18 <158302256+Claudiaam18@users.noreply.github.com>
2025-05-15 20:38:57 -0600

    Ejercicio 1

--- src/main/java/ed/lab/E01TopKFrequentElements.java
+++ src/main/java/ed/lab/E01TopKFrequentElements.java
@@ -1,7 +1,37 @@
 package ed.lab;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class E01TopKFrequentElements {
     public int[] topKFrequent(int[] nums, int k) {
-        return null;
+        Map<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer>[] buckets = new List[nums.length + 1];
+        for(int i = 0; i <= nums.length; i++){
+            buckets[i] = new ArrayList<>();
+        }
+
+        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+            int number = entry.getKey();
+            int freq = entry.getValue();
+            buckets[freq].add(number);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = buckets.length -1; i >= 0 && result.size() < k; i--) {
+            result.addAll(buckets[i]);
+        }
+
+        int[] topK = new int[k];
+        for (int i = 0; i < k; i++) {
+            topK[i] = result.get(i);
+        }
+        return topK;
     }
 }
